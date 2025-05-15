590de707c531d8a78946e0c662db3cd463cc2b30
Sanskaar <sanskaarundale@gmail.com>
2025-05-15 22:03:52 +0530

    login/signup routes

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, Blueprint
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+from routes.auth import auth
+
+app.register_blueprint(auth)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
